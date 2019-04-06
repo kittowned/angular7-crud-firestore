45be977ea34d4c1d0673ae889e5975eab33915ff
Anton Safranovski <kittowned@gmail.com>
2019-04-06 23:00:46 +0300

    Moved some services to SharedModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,29 @@
 import { NgModule } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
-import { BsDropdownModule, ModalModule, BsDatepickerModule } from 'ngx-bootstrap';
-import { PERFECT_SCROLLBAR_CONFIG, PerfectScrollbarConfigInterface, PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
+import { BsDatepickerModule, BsDropdownModule, ModalModule } from 'ngx-bootstrap';
+import { PerfectScrollbarConfigInterface, PerfectScrollbarModule, PERFECT_SCROLLBAR_CONFIG } from 'ngx-perfect-scrollbar';
 import { environment } from 'src/environments/environment';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CustomerListComponent } from './components/customer-list/customer-list.component';
 import { HoursComponent } from './components/hours/hours.component';
+import { ModalComponent } from './components/modal/modal.component';
 import { OrderFormComponent } from './components/order-form/order-form.component';
 import { OrderListComponent } from './components/order-list/order-list.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { RecipeListComponent } from './components/recipe-list/recipe-list.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { OrderService } from './shared/services/order.service';
-import { ProductService } from './shared/services/product.service';
-import { ModalComponent } from './components/modal/modal.component';
-import { FormsModule } from '@angular/forms';
+import { SharedModule } from './shared/shared.module';
 
 
 const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
   suppressScrollX: true
 };
-
 
 @NgModule({
   declarations: [
@@ -50,11 +47,10 @@ const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
     AppRoutingModule,
     ModalModule.forRoot(),
     FormsModule,
-    BsDatepickerModule.forRoot()
+    BsDatepickerModule.forRoot(),
+    SharedModule.forRoot(),
   ],
   providers: [
-    ProductService,
-    OrderService,
     AngularFirestore,
     {
       provide: PERFECT_SCROLLBAR_CONFIG,
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,9 @@
-import { ZippyAccordionComponent } from './components/zippy-accordion/zippy-accordion.component';
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ZippyAccordionComponent } from './components/zippy-accordion/zippy-accordion.component';
+import { OrderService } from './services/order.service';
+import { ProductService } from './services/product.service';
+import { ModuleWithProviders } from '@angular/compiler/src/core';
 
 @NgModule({
   declarations: [
@@ -8,6 +11,19 @@ import { CommonModule } from '@angular/common';
   ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    ZippyAccordionComponent
   ]
 })
-export class SharedModule { }
+export class SharedModule { 
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: SharedModule,
+      providers: [    
+        ProductService,
+        OrderService 
+      ]
+    }
+  }
+}
